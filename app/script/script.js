1b131ab72da84611c239ffd2e666ccf8a70ef74d
Gaius <ogbegbubemi15@gmail.com>
2022-03-06 21:44:17 +0100

    Complete Mobile Header. Add Hamburger menu toggle animation.

--- app/script/script.js
+++ app/script/script.js
@@ -1,1 +1,8 @@
-console.log('hello World!');
+const btnHamburger = document.querySelector("#btnHamburger");
+btnHamburger.addEventListener("click", () => {
+	if (btnHamburger.classList.contains("header__burger--open")) {
+		btnHamburger.classList.remove("header__burger--open");
+	} else {
+		btnHamburger.classList.add("header__burger--open");
+	}
+});
