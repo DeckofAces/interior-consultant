550f70afe08a9addecab7239f3b9903c4a225611
Gaius <ogbegbubemi15@gmail.com>
2022-03-07 00:44:02 +0100

    Complete Mobile Hamburger Menu. Add Toggle functionality. Menu shows and hides on toggle.

--- app/script/script.js
+++ app/script/script.js
@@ -1,8 +1,13 @@
 const btnHamburger = document.querySelector("#btnHamburger");
+const overlay = document.querySelector("#overlay");
+
 btnHamburger.addEventListener("click", () => {
 	if (btnHamburger.classList.contains("header__burger--open")) {
 		btnHamburger.classList.remove("header__burger--open");
+		overlay.style.display = "none";
+		
 	} else {
 		btnHamburger.classList.add("header__burger--open");
+		overlay.style.display = "block";
 	}
 });
